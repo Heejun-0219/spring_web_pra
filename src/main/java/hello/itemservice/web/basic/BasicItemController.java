86f31d035e53ecea72ffbf5d96a93553bebdcff0
Heejun-0219 <heejun0219@gmail.com>
2024-01-24 16:19:03 +0900

    - 상품 등록 처리 - @ModelAttribute
    POST - HTML Form**
    `content-type: application/x-www-form-urlencoded`
    메시지 바디에 쿼리 파리미터 형식으로 전달 `itemName=itemA&price=10000&quantity=10` 예) 회원 가입, 상품 주문, HTML Form 사용
    요청 파라미터 형식을 처리해야 하므로 `@RequestParam` 을 사용하자
    
    /**
    * @ModelAttribute("item") Item item
    * model.addAttribute("item", item); 자동 추가 */
    
    **@ModelAttribute - 요청 파라미터 처리**
    `@ModelAttribute` 는 `Item` 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로 입력해준다.
    **@ModelAttribute - Model 추가**
    `@ModelAttribute` 는 중요한 한가지 기능이 더 있는데, 바로 모델(Model)에 `@ModelAttribute` 로 지정한 객체
    를 자동으로 넣어준다. 지금 코드를 보면 `model.addAttribute("item", item)` 가 주석처리 되어 있어도 잘 동 작하는 것을 확인할 수 있다.
    모델에 데이터를 담을 때는 이름이 필요하다. 이름은 `@ModelAttribute` 에 지정한 `name(value)` 속성을 사용한 다. 만약 다음과 같이 `@ModelAttribute` 의 이름을 다르게 지정하면 다른 이름으로 모델에 포함된다.
    `@ModelAttribute("hello") Item item` 이름을 `hello` 로지정 `model.addAttribute("hello", item);` 모델에 `hello` 이름으로저장

--- src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -8,9 +8,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/basic/items")
@@ -38,9 +40,42 @@ public class BasicItemController {
         return "basic/addForm";
     }
 
+    //    @PostMapping("/add")
+    public String addItemV1(@RequestParam("itemName") String itemName,
+                            @RequestParam("price") int price,
+                            @RequestParam("quantity") Integer quantity,
+                            Model model) {
+        Item item = new Item(itemName, price, quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+        return "basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item) {
+        itemRepository.save(item);
+
+//        model.addAttribute("item", item);
+//        ModelAttribute = 지정해준 이름의 객체로 자동으로 해당 부분에 추가해준다. @ModelAttribute("")에 이름을 설명해주어야한다.
+//        즉, 요청 파라미터 처리와, Model를 추가해주는 기능을 해준다.
+        return "basic/item";
+    }
+
+    //    @PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+        itemRepository.save(item);
+        // Item -> item : modelAttribute("item", item)으로 앞문자를 소문자로 바꾸어서 자동으로 넣어준다.
+
+        return "basic/item";
+    }
+
     @PostMapping("/add")
-    public String save() {
-        return "basic/addForm";
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+
+        return "basic/item";
     }
 
     @PostConstruct
